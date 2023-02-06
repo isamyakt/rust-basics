db5eceeb408a5071c7f10702117c3df652864b14
isamyakt <isamyakt17@gmail.com>
2023-02-06 22:15:35 +0530

    macros default()

--- src/logics/macros.rs
+++ src/logics/macros.rs
@@ -14,3 +14,17 @@ pub fn macros() {
     let items = create_vec!(1, 2, 3);
     println!("{items:?}");
 }
+
+
+
+#[derive(Default)]
+struct MyType {
+    name: String,
+    items: Vec<i32>,
+}
+
+pub fn my_type() {
+    let v = MyType::default();
+    assert!(v.name.is_empty());
+    assert!(v.items.is_empty());
+}
