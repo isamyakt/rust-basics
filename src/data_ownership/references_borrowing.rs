b63c10c76c3caf6569f0401123a238d8a595d535
isamyakt <isamyakt17@gmail.com>
2023-02-13 18:57:09 +0530

    duplicating data: clone()

--- src/data_ownership/references_borrowing.rs
+++ src/data_ownership/references_borrowing.rs
@@ -8,3 +8,9 @@ pub fn main_fn() {
     let len = length_of_string(s1);
     println!("The length is {len}.");
 }
+
+// pub fn main_fn_error() {
+//     let s1 = String::from("Hey there!");
+//     let len = length_of_string(s1);
+//     println!("The length of {s1:?} is {len}.");
+// }
