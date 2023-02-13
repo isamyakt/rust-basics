97a1767b41348cfaf97b64cb91fc1e01ceee8c20
isamyakt <isamyakt17@gmail.com>
2023-02-13 19:03:17 +0530

    duplicating data: clone()

--- src/data_ownership/references_borrowing.rs
+++ src/data_ownership/references_borrowing.rs
@@ -9,8 +9,15 @@ pub fn main_fn() {
     println!("The length is {len}.");
 }
 
-// pub fn main_fn_error() {
+pub fn main_fn_error() {
+    // moved value cannot be used, so thats the error
 //     let s1 = String::from("Hey there!");
 //     let len = length_of_string(s1);
 //     println!("The length of {s1:?} is {len}.");
-// }
+
+    // s1 to the length_of_string function is by duplicating the data first
+    let s1 = String::from("Hey there!");
+    let s2 = s1.clone();
+    let len = length_of_string(s2);
+    println!("The length of {s1:?} is {len}.");
+}
